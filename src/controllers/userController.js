0e7a5e033535f5151e641169269aa57e7672e37c
Rodolphe <dutchr@live.fr>
2021-02-24 14:54:59 +0100

    routing system work fine for register & login but need optimization

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,9 +4,11 @@ import jwt from "jwt-simple";
 const register = async (req, res, next) => {
   try {
     let newUser = new User(req.body);
-    const salt = bcrypt.genSaltSync(process.env.salt);
-    newUser.hash_password = bcrypt.hashSync(req.body.password, salt);
+    const salt = bcrypt.genSaltSync(10);
+    newUser.hash_password = await bcrypt.hashSync(req.body.password, salt);
     newUser = await newUser.save();
+    const token = jwt.encode({ id: newUser.id }, process.env.secret);
+    return res.send({ newUser, token });
   } catch (err) {
     next(err);
   }
@@ -16,7 +18,7 @@ const login = async (req, res, next) => {
   try {
     const email = req.body.email;
     const password = req.body.password;
-    const user = await User.findOne({ email: email, password: password });
+    const user = await User.findOne({ email: email }).select("+password");
     const token = jwt.encode({ id: user.id }, process.env.secret);
     return res.send({ user, token });
   } catch (err) {

975fc1848af616c15c5cb416787d9297c175e187
Rodolphe <dutchr@live.fr>
2021-02-24 13:37:52 +0100

    take care of security basics

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
 const app = express();
-dotenv.config()
+dotenv.config();
 
-const port = process.env.PORT
+const port = process.env.PORT;
+app.use(helmet())
+app.use(helmet.noCache())
+app.use(helmet.frameguard())
+// app.use(helmet({
+//   frameguard:false
+// }))
 
-app.listen(port, () => console.log(`Application is running on port : ${port}`))
+
+app.listen(port, () => console.log(`Application is running on port : ${port}`));
